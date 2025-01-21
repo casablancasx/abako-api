97ecd681995a6c4e120762a69cb2917436fdd585
Danilo Chaves <danchavesds@gmail.com>
2025-01-21 15:18:53 -0300

    initial commit

---
+++ src/main/java/br/gov/agu/abakoapi/AbakoApiApplication.java
@@ -0,0 +1,13 @@
+package br.gov.agu.abakoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbakoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbakoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/agu/abakoapi/AbakoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.agu.abakoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbakoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
